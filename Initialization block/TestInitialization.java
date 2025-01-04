7a641245a52d9c9e64a1486414f8ed2bf8212ed1
het52004 <hetgajjar332@gmail.com>
2025-01-04 22:24:58 +0530

    minor fixes

--- Initialization block/TestInitialization.java
+++ Initialization block/TestInitialization.java
@@ -1,14 +1,15 @@
 public class TestInitialization {
     static {
-        System.out.println("Static block");
+        System.out.println("Static block"); // gets called even if object is not created
     }
     {
-        System.out.println("Instance block");
+        System.out.println("Instance block"); // gets called when object is created
     }
     TestInitialization() {
-        System.out.println("Constructor");
+        System.out.println("Constructor"); // gets called when object is created
     }
     public static void main(String[] args) {
-        TestInitialization t1 = new TestInitialization();
+        System.out.println("Main method");
+        TestInitialization obj = new TestInitialization();
     }
 }
