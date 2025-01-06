74542cbe7d27c0b80497c9cefe4cea28d48f353d
het52004 <hetgajjar332@gmail.com>
2025-01-06 20:55:32 +0530

    minor changes

--- Overloading and overriding/Overloading.java
+++ Overloading and overriding/Overloading.java
@@ -7,17 +7,19 @@ class A {
     }
 }
 class B extends A{
-    
+    public void overloadingInAClass(int a){
+        System.out.println("Overloading of A class overloaded method in B class with int parameter");
+    }
 }
 
 public class Overloading {
     public static void main(String[] args) {
-        // A a = new A();
-        // a.overloadingInAClass();
-        // a.overloadingInAClass(1);
+        A a = new A();
+        a.overloadingInAClass();
+        a.overloadingInAClass(1);
 
-        // B b = new B();
-        // b.overloadingInAClass();
-        // b.overloadingInAClass(1);
+        B b = new B();
+        b.overloadingInAClass();
+        b.overloadingInAClass(1);
     }
 }
