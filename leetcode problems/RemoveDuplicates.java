0903b68df195c3f457f81bba874bb59bb722390b
het52004 <hetgajjar332@gmail.com>
2025-01-11 23:25:05 +0530

    solved duplicate array elements problem

--- leetcode problems/RemoveDuplicates.java
+++ leetcode problems/RemoveDuplicates.java
@@ -1,25 +1,20 @@
 public class RemoveDuplicates {
     public static void main(String[] args) {
-        int nums[] = { 1, 1, 2, 2, 3 };
+        int nums[] = { 1, 1, 1, 2, 2, 3 };
         System.out.println(removeDuplicates(nums));
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
     }
 
     public static int removeDuplicates(int[] nums) {
-        int k = 0, visited = nums[0];
+        int k = 0;
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length - 1; j++) {
-                if (visited == nums[i] && i != 0) {
-                    break;
-                } else {
-                    if (nums[i] == nums[j + 1]) {
-                        nums[j + 1] = nums[j + 2];
-                        nums[j + 2] = 0;
-                        k++;
-                    }
-                }
+            if (nums[i] != nums[k]) {
+                k++;
+                nums[k] = nums[i];
             }
-            visited = nums[i];
         }
-        return k;
+        return (k + 1);
     }
 }
