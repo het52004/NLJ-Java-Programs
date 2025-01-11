7c6ea27b748a1311f748d5c22c4bb95b0a14a672
het52004 <hetgajjar332@gmail.com>
2025-01-11 20:47:54 +0530

    modified arrays file in practice programs and added new function in it

--- Practice prorgams/Arrays.java
+++ Practice prorgams/Arrays.java
@@ -5,10 +5,11 @@ public class Arrays {
         Scanner sc = new Scanner(System.in);
         int arr[] = new int[5];
         int choice;
-        while(true){
-            System.out.print("1. Add Elements\n2. Print Array\n3. Largest Element\n4. Second largest element\n5. Exit\nEnter your choice: ");
+        while (true) {
+            System.out.print(
+                    "1. Add Elements\n2. Print Array\n3. Largest Element\n4. Second largest element\n5. Sorting an array\n6. Exit\nEnter your choice: ");
             choice = sc.nextInt();
-            switch(choice){
+            switch (choice) {
                 case 1:
                     addElements(arr, sc);
                     break;
@@ -22,6 +23,9 @@ public class Arrays {
                     System.out.println("\nSecond Largest Element: " + secondLargestElement(arr) + "\n");
                     break;
                 case 5:
+                    sortArray(arr);
+                    break;
+                case 6:
                     sc.close();
                     System.exit(0);
                 default:
@@ -36,6 +40,7 @@ public class Arrays {
         }
         System.out.println();
     }
+
     public static void addElements(int arr[], Scanner sc) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print("Enter a[" + i + "] = ");
@@ -43,6 +48,7 @@ public class Arrays {
         }
         System.out.println();
     }
+
     public static int largestElement(int arr[]) {
         int max = arr[0];
         for (int i = 1; i < arr.length; i++) {
@@ -52,14 +58,29 @@ public class Arrays {
         }
         return max;
     }
+
     public static int secondLargestElement(int arr[]) {
         int max = largestElement(arr);
         int secondMax = arr[0];
-        for(int i = 1; i < arr.length; i++){
-            if(arr[i] > secondMax && arr[i] < max){
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > secondMax && arr[i] < max) {
                 secondMax = arr[i];
             }
         }
         return secondMax;
+    }
+
+    public static void sortArray(int arr[]) {
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length - 1; j++) {
+                if (arr[i] > arr[j + 1]) {
+                    temp = arr[i];
+                    arr[i] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("The array has been sorted!\n");
     }
 }
