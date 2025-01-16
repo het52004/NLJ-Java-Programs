96cb6d93c97f29c6dd63c80af98c8c2f919f5d8c
het52004 <hetgajjar332@gmail.com>
2025-01-16 20:33:30 +0530

    jagged array in java

--- leetcode problems/RemoveDuplicates.java
+++ leetcode problems/RemoveDuplicates.java
@@ -1,6 +1,6 @@
 public class RemoveDuplicates {
     public static void main(String[] args) {
-        int nums[] = { 1, 1, 1, 2, 2, 3 };
+        int nums[] = { 1, 1, 1, 2, 2, 3, 4, 4, 5, 6 };
         System.out.println(removeDuplicates(nums));
         for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + " ");
