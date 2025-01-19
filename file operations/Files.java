b8bbd4bff712c52ea3cddde412c3ca50662cc9a9
het52004 <hetgajjar332@gmail.com>
2025-01-19 21:21:31 +0530

    added FileMethods file in which all File class methods and its uses are listed

--- file operations/Files.java
+++ file operations/Files.java
@@ -1,10 +1,13 @@
 import java.util.*;
 import java.io.*;
+import java.net.URI;
 
 public class Files {
     public static void main(String[] args) {
         File file = new File("sum.txt");
         System.out.println(file.canWrite());
         System.out.println(file.getName());
+        URI fileURI = file.toURI();
+        System.out.println(fileURI);
     }
 }
