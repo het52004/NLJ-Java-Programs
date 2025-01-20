0f677704edcda4dc3d94d42313487ed3d8f29227
het52004 <hetgajjar332@gmail.com>
2025-01-20 20:46:41 +0530

    leetcode problems

---
+++ leetcode problems/ReverseNumber.java
@@ -0,0 +1,17 @@
+public class ReverseNumber {
+    public static void main(String[] args) {
+        System.out.println(reverse(1534236469));
+    }
+    public static int reverse(int x) {
+        if(x < Integer.MIN_VALUE || x > Integer.MAX_VALUE - 1){
+            return 0;
+        }
+        int reverse = 0, remainder;
+        while (x != 0) {
+            remainder = x % 10;
+            reverse = (reverse * 10) + remainder;
+            x = x / 10;
+        }
+        return reverse;
+    }
+}
