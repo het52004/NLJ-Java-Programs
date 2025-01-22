261591635ab3a90215272c2582ff3ed33698065a
het52004 <hetgajjar332@gmail.com>
2025-01-22 20:25:30 +0530

    minor changes

--- Practice prorgams/MatrixMultiplication.java
+++ Practice prorgams/MatrixMultiplication.java
@@ -7,7 +7,7 @@ public class MatrixMultiplication {
         };
 
         int[][] matrix2 = {
-            {9, 8, 7},
+            {9, 6, 7},
             {6, 5, 4},
             {3, 2, 1}
         };
