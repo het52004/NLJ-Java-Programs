71b6eb0023ca903f4e62d99142629d49f2d243bd
het52004 <hetgajjar332@gmail.com>
2025-01-22 20:25:55 +0530

    changes in MM program

--- Practice prorgams/MatrixMultiplication.java
+++ Practice prorgams/MatrixMultiplication.java
@@ -2,7 +2,7 @@ public class MatrixMultiplication {
     public static void main(String[] args) {
         int[][] matrix1 = {
             {1, 2, 3},
-            {4, 5, 6},
+            {4, 5, 1},
             {7, 8, 9}
         };
 
