690c26a9ece738d4b33ebf97e59607acab7b1b3d
het52004 <hetgajjar332@gmail.com>
2025-01-29 21:39:10 +0530

    multithreading in java

---
+++ multithreading/Multithreading.java
@@ -0,0 +1,24 @@
+public class Multithreading {
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+        a.start();
+        b.start();
+    }
+}
+
+class A extends Thread{
+    public void run(){
+        for(int i = 0; i <= 10; i++){
+            System.out.println("Thread a i = " + i);
+        }
+    }
+}
+
+class B extends Thread{
+    public void run(){
+        for(int i = 0; i <= 10; i++){
+            System.out.println("Thread b i = " + i);
+        }
+    }
+}
